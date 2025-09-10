7bb3459d43be2af984727ac53e4b732552c80428
Alexander-v22 <av275@duke.edu>
2025-09-10 12:51:26 -0400

    UART fix

--- pi-5/esp_comms/uart_con.py
+++ pi-5/esp_comms/uart_con.py
@@ -1,14 +1,14 @@
 import serial
 import time  
 
-ser = serial.Serial("/dev/ttyAMA10", 115200)
+ser = serial.Serial("/dev/serial0", 115200)
 
 time.sleep(2)
 
-while true: 
+while True: 
     received_data = ser.read()
-    sleep(0.03)
     data_left = ser.inWaiting()
     received_data +=ser.read(data_left) # read the data on the Serial Port 
     print (received_data)
     ser.write(received_data) #transmit/ send data on Serial Port
+    time.sleep(0.03)
