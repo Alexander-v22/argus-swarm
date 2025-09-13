2e7819730fb888026f5c339a7f47ba66345e7a58
Alexander-v22 <av275@duke.edu>
2025-09-13 19:43:48 -0400

    fixing pi

--- pi-5/main.py
+++ pi-5/main.py
@@ -7,6 +7,7 @@ def main():
     parser.add_argument('--model', required=True, help='Path to YOLO model file (example: "runs/detect/train/weights/best.pt")')
     parser.add_argument('--thresh', type=float, default=0.5, help='Minimum confidence threshold')
     parser.add_argument('--resolution', default=None, help='Resolution in WxH (example: "640x480")')
+    
 
     args = parser.parse_args()
     yolo_detect.start_detection(args)
