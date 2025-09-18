fb5fb50e125d0e8fc46cd1b8830c6b3fbc25d317
Alexander-v22 <av275@duke.edu>
2025-09-18 14:55:49 -0400

    fixing uart +servo logic

--- pi-5/esp_comms/uart_con.py
+++ pi-5/esp_comms/uart_con.py
@@ -24,11 +24,11 @@ class ESP32UART:
         self.ser.setRTS(True)
 
         # self --> refrence to the object itself
-        def send_angles(self, pan, tilt):
-            msg = f"{int(pan)},{int(tilt)}\n"
-            self.ser.write(msg.encode)
-            self.ser.flush() # forces any data lingering in the output buffer to be sent immediately through the serial port
+    def send_angles(self, pan, tilt):
+        msg = f"{int(pan)},{int(tilt)}\n"
+        self.ser.write(msg.encode)
+        self.ser.flush() # forces any data lingering in the output buffer to be sent immediately through the serial port
 
-        def close(self):
-            self.ser.flush()
-            self.ser.close()
+    def close(self):
+        self.ser.flush()
+        self.ser.close()
