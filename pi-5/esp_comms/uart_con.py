76d2160073566df4b390be33ad2d9a32b950a06f
Alexander-v22 <av275@duke.edu>
2025-09-18 14:54:37 -0400

    fixing uart +servo logic

--- pi-5/esp_comms/uart_con.py
+++ pi-5/esp_comms/uart_con.py
@@ -3,7 +3,7 @@ import time
 
 class ESP32UART:
     def __init__(self, port="/dev/ttyAMA0", baudrate=115200, timeout=1):
-        ser = serial.Serial(
+        self.ser = serial.Serial(
             port,
             baudrate = baudrate,
             bytesize=serial.EIGHTBITS,
@@ -17,14 +17,14 @@ class ESP32UART:
 
         # need these for my UART connection to work 
         time.sleep(0.2)
-        ser.reset_input_buffer()
-        ser.reset_output_buffer()
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
         #setting Data Terminal Ready and Request to Send high like the MINICOM work space
-        ser.setDTR(True)
-        ser.setRTS(True)
+        self.ser.setDTR(True)
+        self.ser.setRTS(True)
 
         # self --> refrence to the object itself
-        def send_angles (self, pan, tilt):
+        def send_angles(self, pan, tilt):
             msg = f"{int(pan)},{int(tilt)}\n"
             self.ser.write(msg.encode)
             self.ser.flush() # forces any data lingering in the output buffer to be sent immediately through the serial port
